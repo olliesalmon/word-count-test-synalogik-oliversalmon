e213856fec9f60f1e658538459d014d02beb3e46
olliesalmon <64041244+olliesalmon@users.noreply.github.com>
2021-02-20 20:56:32 +0000

    *Created classes *IO impl added

---
+++ src/main/java/ui/UserIOImpl.java
@@ -0,0 +1,275 @@
+/*
+ * Implementation of UserIO interface
+ */
+package ui;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Scanner;
+
+public class UserIOImpl implements UserIO {
+
+    Scanner scanner = new Scanner(System.in);
+
+    @Override
+    public void print(String message) {
+        System.out.println(message);
+    }
+
+    @Override
+    public String readString(String prompt) {
+        System.out.println(prompt);
+        return scanner.nextLine();
+    }
+
+    @Override
+    public int readInt(String prompt) {
+        System.out.println(prompt);
+        String stringInput;
+        int input = 0;
+
+        while (true) {
+            try {
+                stringInput = scanner.nextLine();
+                input = Integer.parseInt(stringInput);
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("Enter an integer");
+                break;
+            }
+        }
+        return input;
+    }
+
+    @Override
+    public int readInt(String prompt, int min, int max) {
+        System.out.println(prompt);
+        String stringInput;
+        int input = 0;
+
+        while (true) {
+            try {
+                stringInput = scanner.nextLine();
+                input = Integer.parseInt(stringInput);
+                if (input >= min && input <= max) {
+                    break;
+                } else {
+                    System.out.println("Integer must be between " + min + " and " + max);
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Enter an integer");
+                break;
+            }
+        }
+        return input;
+    }
+
+    @Override
+    public double readDouble(String prompt) {
+        System.out.println(prompt);
+        String stringInput;
+        double input = 0;
+
+        while (true) {
+            try {
+                stringInput = scanner.nextLine();
+                input = Double.parseDouble(stringInput);
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("Enter a double");
+                break;
+            }
+        }
+        return input;
+    }
+
+    @Override
+    public double readDouble(String prompt, double min, double max) {
+        System.out.println(prompt);
+        String stringInput;
+        double input = 0;
+
+        while (true) {
+            try {
+                stringInput = scanner.nextLine();
+                input = Double.parseDouble(stringInput);
+                if (input >= min && input <= max) {
+                    break;
+                } else {
+                    System.out.println("Double must be between " + min + " and " + max);
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Enter a double");
+                break;
+            }
+        }
+        return input;
+    }
+
+    @Override
+    public float readFloat(String prompt) {
+        System.out.println(prompt);
+        String stringInput;
+        float input = 0;
+
+        while (true) {
+            try {
+                stringInput = scanner.nextLine();
+                input = Float.parseFloat(stringInput);
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("Enter a float");
+                break;
+            }
+        }
+        return input;
+    }
+
+    @Override
+    public float readFloat(String prompt, float min, float max) {
+        System.out.println(prompt);
+        String stringInput;
+        float input = 0;
+
+        while (true) {
+            try {
+                stringInput = scanner.nextLine();
+                input = Float.parseFloat(stringInput);
+                if (input >= min && input <= max) {
+                    break;
+                } else {
+                    System.out.println("Float must be between " + min + " and " + max);
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Enter a float");
+                break;
+            }
+        }
+        return input;
+    }
+
+    @Override
+    public long readLong(String prompt) {
+        System.out.println(prompt);
+        String stringInput;
+        long input = 0;
+
+        while (true) {
+            try {
+                stringInput = scanner.nextLine();
+                input = Long.parseLong(stringInput);
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("Enter a long");
+                break;
+            }
+        }
+        return input;
+    }
+
+    @Override
+    public long readLong(String prompt, long min, long max) {
+        System.out.println(prompt);
+        String stringInput;
+        long input = 0;
+
+        while (true) {
+            try {
+                stringInput = scanner.nextLine();
+                input = Long.parseLong(stringInput);
+                if (input >= min && input <= max) {
+                    break;
+                } else {
+                    System.out.println("Long must be between " + min + " and " + max);
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Enter a long");
+                break;
+            }
+        }
+        return input;
+    }
+
+    @Override
+    public BigDecimal readCurrency(String prompt) {
+        System.out.println(prompt);
+        String stringInput;
+        long longInput;
+        BigDecimal input = null;
+
+        while (true) {
+            try {
+                stringInput = scanner.nextLine();
+                longInput = Long.parseLong(stringInput);
+                input = new BigDecimal(Long.toString(longInput));
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("Enter currency as a decimal");
+                break;
+            }
+        }
+        return input;
+    }
+
+    @Override
+    public BigDecimal readCurrency(String prompt, long min, long max) {
+        System.out.println(prompt);
+        String stringInput;
+        long longInput;
+        BigDecimal input;
+
+        while (true) {
+            try {
+                stringInput = scanner.nextLine();
+                longInput = Long.parseLong(stringInput);
+                if (longInput >= min && longInput <= max) {
+                    input = new BigDecimal(Long.toString(longInput));
+                    break;
+                } else {
+                    System.out.println("Value of decimal must be between " + min + " and " + max);
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Enter currency as a decimal");
+            }
+        }
+        return input;
+    }
+
+    @Override
+    public String readLocalDate(String prompt) {
+        LocalDate ld;
+        while (true) {
+            try {
+                String ldString = readString(prompt);
+                ld = LocalDate.parse(ldString, DateTimeFormatter.ofPattern("dd/MM/uuuu"));
+                break;
+            }
+            catch (DateTimeParseException e) {
+                System.out.println("Enter a valid date");
+            }
+        }
+        return ld.format(DateTimeFormatter.ofPattern("MMdduuuu"));
+    }
+
+    @Override
+    public String readFutureLocalDate(String prompt) {
+        LocalDate ld;
+        while (true) {
+            try {
+                String ldString = readString(prompt);
+                ld = LocalDate.parse(ldString, DateTimeFormatter.ofPattern("dd/MM/uuuu"));
+                if (ld.isBefore(LocalDate.now().plusDays(1))) {
+                    System.out.println("Enter a date from tomorrow onwards");
+                    continue;
+                }
+                break;
+            }
+            catch (DateTimeParseException e) {
+                System.out.println("Enter a valid date");
+            }
+        }
+        return ld.format(DateTimeFormatter.ofPattern("MMdduuuu"));
+    }
+}
